c1df797ad3b7881e7ab079a822154f503aa2da2b
Matt Henkes <mjhenkes@gmail.com>
2018-01-04 12:58:23 -0600

    update script

--- rails/scripts/set_all_project_types_to_terra.rb
+++ rails/scripts/set_all_project_types_to_terra.rb
@@ -3,7 +3,27 @@ puts Kaiju::Project.redis.client.host
 # puts ids
 ids.each do |id|
   project = Kaiju::Project.by_id(id)
-  # project.type = terra
+  # project.type = 'terra'
   puts("#{project.id}\n")
   puts("#{project.type.value}\n")
+end
+
+ids = Kaiju::Workspace.ids
+puts Kaiju::Workspace.redis.client.host
+# puts ids
+ids.each do |id|
+  workspace = Kaiju::Workspace.by_id(id)
+  # workspace.project_type = 'terra'
+  puts("#{workspace.id}\n")
+  puts("#{workspace.project_type.value}\n")
+end
+
+ids = Kaiju::Component.ids
+puts Kaiju::Component.redis.client.host
+# puts ids
+ids.each do |id|
+  component = Kaiju::Component.by_id(id)
+  # component.project_type = 'terra'
+  puts("#{component.id}\n")
+  puts("#{component.project_type.value}\n")
 end
