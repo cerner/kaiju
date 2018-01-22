fb8e0dea3eda0c2084d14cd0f23baf105ee0ca37
Stephen Esser <Stephen.Esser@gmail.com>
2018-01-21 22:02:44 -0600

    Updated - Lazy load components

--- rails/app/controllers/components_controller.rb
+++ rails/app/controllers/components_controller.rb
@@ -4,19 +4,19 @@ class ComponentsController < ApplicationController
   before_action { fetch_component(params[:id]) }
   before_action(only: %i[show]) { current_user.add_recent_workspace(params[:project_id], params[:workspace_id]) }
   before_action(except: %i[show attributes]) { authorize(params[:workspace_object].editors) }
+
   # GET /projects/1/workspaces/1/components/1
   def show # rubocop:disable Metrics/AbcSize
     # Rails.logger.debug params
     component = params[:component_object]
-
-    @component = Kaiju::ComponentJson.as_json(
-      component.id, request.base_url,
-      project_id: params[:project_id], workspace_id: params[:workspace_id], user_id: current_user.id
-    )
+    options = { project_id: params[:project_id], workspace_id: params[:workspace_id], user_id: current_user.id }
 
     respond_to do |format|
-      format.html
-      format.json { render json: JSON.pretty_generate(@component) }
+      format.html { @component = Kaiju::ComponentJson.as_json_lite(component.id, request.base_url, options) }
+      format.json do |_json|
+        @component = Kaiju::ComponentJson.as_json(component.id, request.base_url, options)
+        render json: JSON.pretty_generate(@component)
+      end
     end
   end
 
