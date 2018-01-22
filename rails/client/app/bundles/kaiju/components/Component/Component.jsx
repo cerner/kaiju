6bb4ac29a03b401d1829ffc27f7f4eb4e9c5f999
Stephen Esser <Stephen.Esser@gmail.com>
2018-01-22 12:34:34 -0600

    Updated - Lazy load components (#68)
    * Updated - Lazy load components
    
    * Remove reference to request
    
    * Apply ant spin styles manually to prevent global resets

--- rails/client/app/bundles/kaiju/components/Component/Component.jsx
+++ rails/client/app/bundles/kaiju/components/Component/Component.jsx
@@ -1,31 +1,59 @@
 import React from 'react';
 import Base from 'terra-base';
 import PropTypes from 'prop-types';
+import ajax from 'superagent';
+import classNames from 'classnames/bind';
 import { Provider } from 'react-redux';
 import configureStore from './store/componentStore';
 import ComponentContainer from './containers/ComponentContainer';
+import Spinner from '../common/Spinner/Spinner';
 import distpatcher from './utilities/dispatcher';
 import initializeDrag from './utilities/drag';
+import styles from './Component.scss';
+
+const cx = classNames.bind(styles);
 
 const propTypes = {
   /**
-   * The Component Identifer
+   * The Component identifer.
    */
   id: PropTypes.string.isRequired,
+  /**
+   * The Component url.
+   */
+  url: PropTypes.string.isRequired,
 };
 
 class Component extends React.Component {
   constructor(props) {
     super(props);
-    this.store = configureStore(this.props);
+
+    this.state = { loading: true };
   }
 
   componentDidMount() {
-    distpatcher(this.store, this.props.id);
     initializeDrag();
+
+    ajax
+      .get(this.props.url)
+      .set('Accept', 'application/json')
+      .end((error, response) => {
+        if (response) {
+          const component = JSON.parse(response.text);
+
+          this.store = configureStore(component);
+          distpatcher(this.store, this.props.id);
+
+          this.setState({ loading: false });
+        }
+      });
   }
 
   render() {
+    if (this.state.loading) {
+      return <div className={cx('loading')}><Spinner /></div>;
+    }
+
     return (
       <Provider store={this.store}>
         <Base locale="en-US">
