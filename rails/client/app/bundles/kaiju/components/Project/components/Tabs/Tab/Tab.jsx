afbba11a2f1b3ca93eedbe3a8f11c77800a62e99
Stephen Esser <Stephen.Esser@gmail.com>
2018-03-01 16:41:25 -0600

    Updated - Added native hover titles to Cards and Tabs

--- rails/client/app/bundles/kaiju/components/Project/components/Tabs/Tab/Tab.jsx
+++ rails/client/app/bundles/kaiju/components/Project/components/Tabs/Tab/Tab.jsx
@@ -1,51 +1,56 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
+import classNames from 'classnames/bind';
 import IconClose from 'terra-icon/lib/icon/IconClose';
-import './Tab.scss';
+import styles from './Tab.scss';
+
+const cx = classNames.bind(styles);
 
 const propTypes = {
   /**
-   * Content to be displayed within the Tab
+   * Content to be displayed within the Tab.
    */
   children: PropTypes.node,
   /**
-   * Whether the tab is selected
+   * Whether the tab is selected.
    */
   isSelected: PropTypes.bool,
   /**
-   * Callback for click events
+   * Callback for click events.
    */
   onClick: PropTypes.func,
   /**
-   * Callback for double click events
+   * Callback for double click events.
    */
   onDoubleClick: PropTypes.func,
   /**
-   * Callback for requesting to close
+   * Callback for requesting to close.
    */
   onRequestClose: PropTypes.func,
+  /**
+   * Title of the tab.
+   */
+  title: PropTypes.string,
 };
 
-const Tab = ({ children, isSelected, onClick, onDoubleClick, onRequestClose }) => {
+const Tab = ({ children, isSelected, onClick, onDoubleClick, onRequestClose, title }) => {
   /**
-   * Handler for the close button
-   * @param {Object} event - The event invoking the function
-   * @return undefined
+   * Handler for the close button.
+   * @param {event} event - The event invoking the function.
    */
   const handleClose = (event) => {
-    // Prevents the event from bubbling up and invoking onClick
+    // Prevents the event from bubbling up and invoking onClick.
     event.stopPropagation();
     onRequestClose(event);
   };
 
-  const classes = classNames(['kaiju-Tab', { 'is-selected': isSelected }]);
+  const classes = cx('tab', { 'is-selected': isSelected });
   return (
-    <li className={classes} onClick={onClick} onDoubleClick={onDoubleClick} role="tab">
-      <span className="kaiju-Tab-content">
+    <li className={classes} onClick={onClick} onDoubleClick={onDoubleClick} role="tab" title={title}>
+      <span className={cx('content')}>
         {children}
       </span>
-      <div className="kaiju-Tab-closeButton" onClick={handleClose} role="presentation">
+      <div className={cx('close')} onClick={handleClose} role="presentation" title="Close">
         {onRequestClose && <IconClose />}
       </div>
     </li>
