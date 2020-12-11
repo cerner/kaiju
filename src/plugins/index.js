e9bdea9620ff2e4c666c065027a3b0074ca26466
Stephen Esser <stephen.esser@cerner.com>
2020-12-11 15:03:33 -0600

    Add terra-divider to the component catalog

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -6,6 +6,7 @@ import Alert from './terra-alert';
 import Arrange from './terra-arrange';
 import Badge from './terra-badge';
 import Button from './terra-button';
+import Divider from './terra-divider';
 import ContentContainer from './terra-content-container';
 import Placeholder from './terra-sandbox';
 import Tag from './terra-tag';
@@ -23,6 +24,7 @@ export default {
   'terra-card:card': Card,
   'terra-card:card:body': CardBody,
   'terra-content-container:content-container': ContentContainer,
+  'terra-divider:divider': Divider,
   'terra-avatar:generic': GenericAvatar,
   'terra-avatar:facility': FacilityAvatar,
   'terra-sandbox:placeholder': Placeholder,
